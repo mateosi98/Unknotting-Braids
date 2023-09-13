fd45d54c81a5d3db4dc958b13207bf0fd950286a
mateosi98 <51299251+mateosi98@users.noreply.github.com>
2023-09-13 15:37:23 +0100

    Update main_hard_braids.py

--- code/main_hard_braids.py
+++ code/main_hard_braids.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from random import shuffle
 import kitty_lacey
-general_dir_name = '/Users/mateosallesize/Documents/SRO/Braids/Unknotting'
-dir_name = '/Users/mateosallesize/Documents/SRO/Braids/Unknotting'
+general_dir_name = ''
+dir_name = ''
 code_dir = f"/code"
 sys.path.insert(0, dir_name+code_dir)
 
@@ -149,99 +149,3 @@ index_stuck[0]
 
 
 
-
-
-# set[57]
-# set_stuck = [set[index] for index in index_stuck]
-# set_stuck[0]
-
-# n_max = sum([1 for i in trivial_set if i== True])
-# print(n_stack, n_max)
-# len(set)
-# trivial_set[0:5]
-
-
-# tensorboard --logdir logs/2s_10l/braid_knot_env/PPO_0
-# logs/2s_10l/braid_knot_env/PPO_0
-# tensorboard --logdir=logs/2s_10l/braid_knot_env/PPO_0,logs/2s_10l/e1_knot_env/PPO_0
-
-
-
-
-
-
-# a = [-1, -2, -2, -1, -2, -2]
-# greedy_unknotting(a,0,[],len(a), 3)
-
-
-# # braid, steps, stack = greedy_unknotting(set[185],0,[],len(set[185]), 3)
-# def something(b):   
-#     braid, steps, stack = greedy_unknotting(b,0,[],len(b), 3)
-#     for i in stack:
-#         com, b_com = [], []
-#         print(f"KL_comp :{kitty_lacey.complexity(i,s=3)}")
-#         # print(i)
-#         for c in range(len(i)):
-#             a = i.copy()    
-#             inv = a[c] * -1
-#             a[c] = inv
-#             # a[c] = a[c] * -1
-#             b_com.append(a)
-#             com.append(kitty_lacey.complexity(a,s=3))
-#         # print(b_com[np.argmin(com)])
-#         print(com)
-#         # print(np.argmin(com))
-#         # b = i.copy()
-#         # b[np.argmin(com)] *= -1
-#         # print(kitty_lacey.complexity(b,s=3))
-
-# i, b_com, com = [-2, 1, 1, 2, 1, 1, 2, 2], [], []
-# for c in range(len(i)):
-#     a = i.copy()    
-#     inv = a[c] * -1
-#     a[c] = inv
-#     # a[c] = a[c] * -1
-#     b_com.append(a)
-#     com.append(kitty_lacey.complexity(a,s=3))
-
-# something(aw_set[0][:len(aw_set[0])-1])
-
-# s = 3
-# l = 20
-
-# answers = []
-# data_dir = f"/data/"
-# file_dir = f"pure_{s}s_{l}l"
-# kitty_lacey.set_params(s, l)
-# try:
-#     results_df = pd.read_csv(general_dir_name+data_dir+file_dir+'_results.csv').drop('Unnamed: 0',axis=1)
-#     results_df = results_df.loc[:10000]
-#     test_set = results_df.iloc[:, :l].values.tolist()
-# except:
-#     print('NO DATA FOUND')
-# len(test_set[0])
-
-# def test_greedy(data=[], max_steps=100):
-#     info_list, index = [], 0
-#     for braid in data:
-#         # greedy_unknotting(b,0,len(b), 3)[0]
-#         braid, steps, stack,action_log, triv = greedy_unknotting(braid,0,[],[],len(braid), s)
-#         info_list.append([index,triv,steps])
-#         index += 1
-#         print(index)
-#     return info_list
-
-# results_greedy = test_greedy(test_set,100)
-# df_results_greedy = pd.DataFrame(results_greedy, columns=['INDEX','DONE', 'STEPS'])
-# results_df['greedy'] = df_results_greedy.loc[:10000].STEPS.values.tolist()
-# df_results_greedy[df_results_greedy.DONE==True].loc[:10000].STEPS.mean()
-# len(df_results_greedy[df_results_greedy.DONE==True]) /len(df_results_greedy)
-# results_df.to_csv(general_dir_name+data_dir+file_dir+'_results.csv')
-
-# df_results_greedy[df_results_greedy.DONE==True]
-
-# print('Mean_steps: ',df_results_greedy[df_results_greedy.DONE==True]['STEPS'].mean())
-
-# print('STD_steps: ',df_results_greedy[df_results_greedy.DONE==True]['STEPS'].std())
-
-# print("Accuracy: ",len(df_results_greedy[df_results_greedy.DONE==True]) / len(test_set)*100,"%")
